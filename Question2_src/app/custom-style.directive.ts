c5d58a37958d046aa92de9755413955f69c4f45a
komalsuraj <komal.shinde@ambersoft.in>
2024-01-02 17:26:39 +0530

    Assignment 10 is done

---
+++ Question2_src/app/custom-style.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomStyle]',
+  standalone: true
+})
+export class CustomStyleDirective {
+
+  constructor(private obj : ElementRef) { 
+    this.obj.nativeElement.style.background = 'yellow';
+    this.obj.nativeElement.style.fontWeight = 'bold';
+  }
+
+}
